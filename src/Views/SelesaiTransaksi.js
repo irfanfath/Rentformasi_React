56ecf53554c5d13dbfbd9e692564e754e7208c20
irfanfath <mirfanf7@gmail.com>
2020-01-08 00:05:52 +0700

    Update SelesaiTransaksi.js

--- src/Views/SelesaiTransaksi.js
+++ src/Views/SelesaiTransaksi.js
@@ -4,34 +4,18 @@ import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBMask, MDBView } from "mdbreact
 
 const SelesaiTransaksi = () => {
   return (
-    <MDBCard className="my-5 px-5 pb-5">
-      <MDBCardBody>
-        <hr className="mb-5 mt-4" />
-        <MDBRow>
-          <MDBCol md="12">
-            <MDBCard reverse>
-              <MDBView hover cascade waves>
-                <img
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(146).jpg"
-                  alt=""
-                  className="img-fluid"
-                />
-                <MDBMask overlay="white-slight" className="waves-light" />
-              </MDBView>
-              <MDBCardBody cascade className="text-center">
-                <h2 className="font-weight-bold">
-                  <a href="#!">Terima Kasih</a>
-                </h2>
-                <p>Apabila 1 x 24 jam pembayaran belum terkonfirmasi harap hubungi kami</p>
-                <NavLink to="/selesaitransaksi" className="nav-link-product w-nav-link">
-                    <input type="submit" value="Lihat Status Transaksi" className="w-commerce-commerceaddtocartbutton button" />
-                </NavLink>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBCardBody>
-    </MDBCard>
+    <div className="page-wrapper">
+      <div className="section hero-section">
+            <div className="wrapper text-white">
+                <div className="hero-intro"> 
+                    <div className="title">
+                        Apabila 1 X 24 jam pesanan anda belum terkonfirmasi harap hubungi kami 
+                    </div>
+                    <h1>Terima Kasih Atas Kepercayaan Anda</h1><NavLink to="/barang" className="button w-button">Lihat Status Pesanan</NavLink>
+                </div>
+            </div>
+        </div>
+    </div>
   );
 }
 
